2b82944ac41105d4340b34a6763f1ae2e83ef62c
shams-shifl <109410388+shams-shifl@users.noreply.github.com>
2025-04-25 02:09:25 +0600

    added initial codes to files

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "permalist",
+  password: "123456",
+  port: 5432,
+});
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
