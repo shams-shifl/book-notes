fcd55e807fcbbb259a973b1a832c27d1c03dc627
shams-shifl <109410388+shams-shifl@users.noreply.github.com>
2025-04-26 00:17:20 +0600

    Created the html ejs and linked the database.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = 3000;
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
-  database: "permalist",
+  database: "bookNotes",
   password: "123456",
   port: 5432,
 });
@@ -16,6 +16,25 @@ db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+async function checkShelf() {
+  const result = await db.query("SELECT * FROM bookshelf ORDER BY readAt DESC");
+  console.log(result.rows);
+  let bookshelf = [];
+  result.rows.forEach((book) => {
+    bookshelf.push(book);
+  });
+  console.log(bookshelf);
+  return bookshelf;
+}
+
+app.get("/", async (req, res) => {
+  const shelf = await checkShelf();
+  console.log(shelf);
+  res.render("index.ejs", {
+    shelf: shelf,
+  });
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
