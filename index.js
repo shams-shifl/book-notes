2c55dd831e6ea1c52a1ff227ff0c5decbad6e6cd
shams-shifl <109410388+shams-shifl@users.noreply.github.com>
2025-05-07 19:43:10 +0600

    added the Add Book functionalities

--- index.js
+++ index.js
@@ -19,21 +19,44 @@ app.use(express.static("public"));
 
 async function checkShelf() {
   const result = await db.query("SELECT * FROM bookshelf ORDER BY readAt DESC");
-  console.log(result.rows);
+  // console.log(result.rows);
+
   let bookshelf = [];
   result.rows.forEach((book) => {
     bookshelf.push(book);
   });
-  console.log(bookshelf);
+  
+  // console.log(bookshelf);
+
   return bookshelf;
 }
 
 app.get("/", async (req, res) => {
   const shelf = await checkShelf();
-  console.log(shelf);
+
+  // console.log(shelf);
+
   res.render("index.ejs", {
     shelf: shelf,
+    pageTitle: "My Book Shelf",
   });
+});
+
+app.post("/addBook", async (req, res) => {
+  res.render("addBook.ejs", {
+    pageTitle: "Add Book to Shelf",
+  });
+});
+
+app.post("/newBook", async (req, res) => {
+  const newBook = await db.query(
+    "INSERT INTO bookshelf (title, isbn, author, readat, rating, note) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+    [req.body.title, req.body.isbn, req.body.author, req.body.readat, req.body.rating, req.body.note]
+  );
+
+  // console.log(newBook.rows[0]);
+
+  res.redirect("/");
 });
 
 app.listen(port, () => {
